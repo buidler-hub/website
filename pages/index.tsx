ae4aaab2dd8f29d262ddabf9685f4549106aa80a
Anurag <kr.anurag24@gmail.com>
2022-05-08 19:39:00 +0530

    feat: landing page (#1)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,61 @@
 import Header from '../components/Header';
-import { VStack } from '@chakra-ui/react';
+import { VStack, Box, Text, Button, Flex, Image } from '@chakra-ui/react';
 import { FC } from 'react';
 
 const Home: FC = () => {
     return (
-        <VStack w="100vw" h="100vh" bg="#010101">
+        <VStack
+            minW="100vw"
+            minH="100vh"
+            overflowX="hidden"
+            gap="28"
+            bg="#010101"
+            fontFamily="Monument Extended"
+            fontWeight="extrabold"
+        >
             <Header />
+
+            <Box textAlign="center" display="flex" flexDir="column" gap="10">
+                <Text fontSize="48px" lineHeight="50px" color="white">
+                    a shelter for <br />
+                    products crafted <br />
+                    by our team
+                </Text>
+
+                <Flex justifyContent="center">
+                    <Button
+                        w="48"
+                        bgColor="blue.600"
+                        textColor="white"
+                        h="44px"
+                        rounded="lg"
+                        fontSize="md"
+                        _focus={{}}
+                        _hover={{ bgColor: 'blue.500' }}
+                        _active={{}}
+                    >
+                        our projects
+                    </Button>
+                </Flex>
+
+                <Image
+                    src="/assets/blob1.svg"
+                    height="60"
+                    width="60"
+                    position="absolute"
+                    bottom="0"
+                    left="0"
+                />
+
+                <Image
+                    src="/assets/blob2.svg"
+                    height="60"
+                    width="60"
+                    position="absolute"
+                    bottom="0"
+                    right="0"
+                />
+            </Box>
         </VStack>
     );
 };
---
+++ styles/theme.chakra.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    fonts: {
+        MonumentExtended: 'Monument Extended',
+    },
+});
+
+export default theme;
