c110c860a5dc15de3711587c1037a986e860188c
Anurag <kr.anurag24@gmail.com>
2022-06-06 13:36:23 +0000

    feat: project card component done w/ dummy data

--- components/Cards/ProjectCard.card.tsx
+++ components/Cards/ProjectCard.card.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Text } from '@chakra-ui/react';
 import type { NextComponentType, NextPageContext } from 'next';
 
 interface Props {}
@@ -8,20 +8,66 @@ const ProjectCard: NextComponentType<NextPageContext, {}, Props> = () => {
         <Box
             display="flex"
             flexDir="column"
-            rounded="xl"
-            h="96"
+            rounded="lg"
+            h="22rem"
             w="72"
             bgColor="white"
             m="20"
         >
             <Box
-                h="40"
+                h="32"
                 w="full"
-                roundedTop="xl"
+                roundedTop="lg"
                 bgGradient="linear(90deg, #DA22FF 0%, #9733EE 100%)"
+            >
+                <Box
+                    w="28"
+                    h="8"
+                    rounded="lg"
+                    bgColor="#F2F4F766"
+                    m="3"
+                    cursor="pointer"
+                    border="2px solid"
+                    borderColor="#E8EAED66"
+                ></Box>
+            </Box>
+
+            <Box
+                h="20"
+                w="20"
+                rounded="full"
+                border="3px solid"
+                borderColor="white"
+                bgGradient="linear(90deg, #00F5A0 0%, #00D9F5 100%)"
+                position="absolute"
+                ml="6"
+                mt="24"
             ></Box>
 
-            <Box rounded="full" border="3px solid" borderColor="white"></Box>
+            <Text ml="6" fontSize="xl" fontFamily="redHat" mt="12">
+                Title
+            </Text>
+
+            <Text ml="6" fontSize="md" fontFamily="redHat" mt="2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Molestias
+            </Text>
+
+            <Button
+                w="32"
+                h="9"
+                ml="6"
+                my="2"
+                bgColor="#E5E7EBB2"
+                border="2px solid"
+                borderColor="#D1D5DBB2"
+                _hover={{ bgColor: '#D1D5DBB2' }}
+                _focus={{}}
+                _active={{}}
+                transition="all 0.2s"
+            >
+                GitHub
+            </Button>
         </Box>
     );
 };
