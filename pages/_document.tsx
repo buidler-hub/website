63b902689e7c8931d90a8d52c4ac9546d0e13c10
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-06-07 14:41:13 +0530

    feat: add og image, favicon and stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 const Document = () => {
     return (
         <Html lang="en">
-            <Head></Head>
+            <Head>
+                <link rel="icon" href="/assets/logo.png" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
